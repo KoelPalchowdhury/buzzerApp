0c5572767df297f4187c05b5182cdf93272f72cf
KoelPalchowdhury <65625859+KoelPalchowdhury@users.noreply.github.com>
2021-01-07 01:17:52 +0530

    Update App.js
    when we are importing class the syntax should be
    import classname from "filepath with file name"
    in tab navigation constant we have to create tab objects for this case 2 tab objects and each will holds two screens
    so 
    transaction:{screens:facebook};
    syntax
    tabobjectname:{screens: screenclassname}

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tab';
-import TransactionScreen from './Screens/fb';
-import SearchScreen from './Screens/in';
+import facebook from './Screens/Face';
+import instagram from './Screens/Insta';
 
 export default class App extends React.Component{
   render (){
@@ -13,7 +13,7 @@ export default class App extends React.Component{
   }
 }
 const tabNavigator = createBottomTabNavigator({
-  Transaction: {screen:fb},
-  Search:{screen: }
+  Transaction: {screen:facebook},
+  Search:{screen:instagram }
 })
 const AppContainer = createAppContainer(tabNavigator)
